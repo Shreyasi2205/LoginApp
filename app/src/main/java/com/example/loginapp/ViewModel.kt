106315971a480725db43603372a6f0bb7480a1fe
Shreyasi2205 <shreyasi22knp@gmail.com>
2021-11-18 02:57:49 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/ViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.loginapp
+import android.view.View
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ViewModel:ViewModel(){
+    var txtdata="Ok"
+}
